fa2c59b80094c1e1d86e07bd0f823ab62cad8d7e
ayasono <yuehu@ualberta.ca>
2024-06-19 00:16:01 +0900

    add: database connection test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI()
 
+# database params
+conn_params = {
+    'dbname': 'shop',
+    'user': 'postgres',
+    'password': 'qQ7539518462',
+    'host': 'localhost',
+    'port': '5432'    
+}
+
+# connect
+try:
+    conn = psycopg2.connect(**conn_params)
+    cursor = conn.cursor()
+
+    # execute
+    cursor.execute('SELECT version();')
+    db_version = cursor.fetchone()
+    print(f'PostgreSQL database version: {db_version}')
+
+    # close
+    cursor.close()
+    conn.close()
+except Exception as e:
+    print(f'Error: {e}')
+    
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
