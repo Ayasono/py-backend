bc9df3b8b8f957c466744fc619e4a9dae6b8de23
ayasono <yuehu@ualberta.ca>
2024-06-27 00:31:12 +0900

    feat: get product by path variable

--- app/api/v1/endpoints/product.py
+++ app/api/v1/endpoints/product.py
@@ -8,14 +8,22 @@ router = APIRouter()
 
 
 @router.get("/")
-def read_products(db: Session = Depends(get_db)):
+async def read_products(db: Session = Depends(get_db)):
     products = db.query(Product).all()
     products_data = [[product.sku, product.name] for product in products]
     return ResponseBase(code=200, message="ok", data=products_data)
 
 
+@router.get("/item/{sku}")
+async def read_product(sku: str, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.sku == sku).first()
+    if not product:
+        return ResponseBase(code=404, message="Product not found", data=None)
+    return ResponseBase(code=200, message="ok", data=product.name)
+
+
 @router.post("/")
-def create_product(name: str, db: Session = Depends(get_db)):
+async def create_product(name: str, db: Session = Depends(get_db)):
     product = Product(name=name)
     db.add(product)
     db.commit()
