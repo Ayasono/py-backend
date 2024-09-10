989bb838481ab6fd1152b3c2e3f13d8f4e25f7df
ayasono <yuehu@ualberta.ca>
2024-09-11 00:13:13 +0900

    fix: order_details design

--- app/models/order_details.py
+++ app/models/order_details.py
@@ -5,9 +5,10 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float
 class OrderDetails(Base):
     __tablename__ = 'order_details'
 
-    order_id = Column(Integer, primary_key=True, index=True)
-    sku = Column(String, ForeignKey('product.sku'))
+    order_id = Column(Integer, ForeignKey('orders.order_id'), index=True)
+    sku = Column(String, ForeignKey('product.sku'), index=True)
     quantity = Column(Integer, nullable=False, default=1)
-    original_price = Column(Integer, nullable=False)
-    discount = Column(Integer, nullable=False, default=0)
+    original_price = Column(Float, nullable=False)
+    discount_percentage = Column(Float, nullable=False, default=0)
+    discount_amount = Column(Float, nullable=False, default=0)
     total = Column(Float, nullable=False)
